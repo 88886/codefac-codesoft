cd9815fa410b79820e2aab8c5d2d1c86c3d1a9e7
carlo <carlo@DESKTOP-G0P5HKM>
2016-01-18 11:20:43 -0500

    agregando cambios en la plantilla y el login

---
+++ codefac-ejb/src/main/java/ec/com/codesoft/modelo/facade/UsuarioFacade.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.com.codesoft.modelo.facade;
+
+import ec.com.codesoft.modelo.Usuario;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author carlo
+ */
+@Stateless
+public class UsuarioFacade extends AbstractFacade<Usuario>
+{
+    @PersistenceContext(unitName = "codefacPU")
+    private EntityManager em;
+
+    public UsuarioFacade() 
+    {
+        super(Usuario.class);
+    }
+    
+    @Override
+    protected EntityManager getEntityManager() 
+    {
+        return em;
+    }
+    
+        /**
+     * Verifica si los datos del usuario son correctos
+     *
+     * @param nick nombre de usuario
+     * @param clave clave de la cuenta
+     * @return
+     */
+    public Usuario login(String nick, String clave) {
+        
+        String queryString = "SELECT u FROM Usuario u where u.nick=?1 And u.clave=?2";
+        Query query = em.createQuery(queryString);
+        query.setParameter(1,nick);
+        query.setParameter(2,clave);
+        Usuario usuario = (Usuario) query.getSingleResult();
+        return usuario;
+    }
+    
+}
