759ee5ec8bf1c7d076d3196ead6a8d13d438e3d5
Suco <suco.pao@gmail.com>
2016-05-13 13:31:58 -0500

    respaldo de db creado

---
+++ codefac-web/src/main/java/ec/com/codesoft/web/admin/backup/backupMB.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.com.codesoft.web.admin.backup;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
+
+/**
+ *
+ * @author Suco
+ */
+@ManagedBean
+public class backupMB {
+
+    ServletContext ctx;
+    String basePath;
+    // private String txtPath = "G:\\New Folder\\";
+    private String txtPath;
+    private String lblMessage;
+    
+    @PostConstruct
+    public void inicializar(){
+        ctx = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();
+        basePath = ctx.getRealPath("/");
+        txtPath = basePath + "BackupRestoreMysqldb//";
+        System.out.println("Path "+ txtPath);
+    }
+
+    public void backup() {
+        if (txtPath.equals("")) {
+            lblMessage = ("Please choose path to save!");
+        } else {
+            Date now = new Date();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy_HH-mm-ss");
+            //File file = new File(dateFormat.format(now));
+            String strFilename = dateFormat.format(now);
+
+            long nowLong = now.getTime();
+            //String strFilename;
+            //strFilename = nowLong.toString();
+            //strFilename = String.valueOf(nowLong);
+            System.out.println(strFilename);
+              //String command = "C://xampp//mysql//bin/mysqldump -u(db user name) -p(db password) --add-drop-database -B (db name) -r " + "\"" + txtPath.getText().toString() + "\\" + strFilename + ".sql\"";
+            String command = "C://xampp//mysql//bin/mysqldump --user=root --password=root --add-drop-database -B codefac -r " + "\"" + txtPath.toString() + "\\" + strFilename + ".sql\"";
+            System.out.println(command);
+            Process p = null;
+            try {
+                Runtime runtime = Runtime.getRuntime();
+                p = runtime.exec(command);
+
+                int processComplete = p.waitFor();
+
+                if (processComplete == 0) {
+
+                    // System.out.println("<html><font color='green'>Backup created successfully!</font></html>");
+                    lblMessage = "Backup created successfully to " + txtPath.toString() + "\\" + strFilename + ".sql";
+
+                } else {
+                    lblMessage = "Could not create the backup";
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getLblMessage() {
+        return lblMessage;
+    }
+
+    public void setLblMessage(String lblMessage) {
+        this.lblMessage = lblMessage;
+    }
+    
+    
+}
