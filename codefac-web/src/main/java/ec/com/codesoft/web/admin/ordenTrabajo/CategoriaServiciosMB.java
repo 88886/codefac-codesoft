701c4bc827e426a1ebc1110d11b85619ca949e5d
carlosmast23 <carlosmast2301@hotmail.es>
2016-06-03 16:53:18 -0500

    Corregido estilos de las ordenes de trabajo

--- codefac-web/src/main/java/ec/com/codesoft/web/admin/ordenTrabajo/CategoriaServiciosMB.java
+++ codefac-web/src/main/java/ec/com/codesoft/web/admin/ordenTrabajo/CategoriaServiciosMB.java
@@ -23,73 +23,113 @@ import org.primefaces.context.RequestContext;
  */
 @ViewScoped
 @ManagedBean
-public class CategoriaServiciosMB implements Serializable{
-    
-    
+public class CategoriaServiciosMB implements Serializable {
+
     @EJB
     private ServiciosServicio serviciosServicio;
-    
+
     @EJB
     private CategoriaServicioServicios categoriaServicio;
-    
+
     /**
      * Lista para mostrar en la tabla
      */
     private List<CategoriaTrabajo> categoriaTrabajoList;
-    
+
     private List<Servicios> serviciosList;
-    
-    
+
     /**
      * Servicio Seleccionado
      */
     private Servicios servicioSeleccionado;
-    
-    /***
+
+    /**
+     * *
      * Categoria para seleccionar en la tabla
      */
     private CategoriaTrabajo categoriaSeleccionada;
-    
+
     /**
      * Variable para crear l nueva categoria
      */
     private CategoriaTrabajo categoriaNuevo;
-    
+
     /**
      * id del servicio para crear y editar
      */
     private Integer idServicioSeleccionado;
     
-    
+
+    private Servicios servicioCrear;
+            
     @PostConstruct
-    public void postCostruct()
-    {
-        categoriaTrabajoList=categoriaServicio.obtenerTodos();
+    public void postCostruct() {
+        categoriaTrabajoList = categoriaServicio.obtenerTodos();
     }
-    
-    
-    public void abrirNuevoCatalogo()
-    {
-        System.out.println("abriendo dialogo nuevo servicio");
-        categoriaNuevo=new CategoriaTrabajo();
-        serviciosList=serviciosServicio.obtenerTodos();
+
+    public void abrirNuevoCatalogo() {
+        System.out.println("abriendo dialogo nuevo catalago");
+        categoriaNuevo = new CategoriaTrabajo();
+        serviciosList = serviciosServicio.obtenerTodos();
         RequestContext.getCurrentInstance().execute("PF('widgetNuevoCatalogo').show()");
     }
-    
-    public void guardarCatalogo()
-    {
+
+    public void guardarCatalogo() {
         System.out.println("guardar el catalogo");
-        Servicios servicio=serviciosServicio.buscarPorID(idServicioSeleccionado);
+        Servicios servicio = serviciosServicio.buscarPorID(idServicioSeleccionado);
         categoriaNuevo.setCodigoServicio(servicio);
         categoriaServicio.grabar(categoriaNuevo);
         RequestContext.getCurrentInstance().execute("PF('widgetNuevoCatalogo').hide()");
+        categoriaTrabajoList = categoriaServicio.obtenerTodos();
+
+    }
+
+    public void abrirEditarCatalogo() {
+        System.out.println("abriendo dialogo editar servicio");
+        idServicioSeleccionado=categoriaSeleccionada.getCodigoServicio().getCodigoServicio();
         categoriaTrabajoList=categoriaServicio.obtenerTodos();
+                
+        RequestContext.getCurrentInstance().execute("PF('widgetEditarServicio').show()");
+    }
+    
+    public void abrirNuevoServicio()
+    {
+        System.out.println("dialogo new servicio");
+        servicioCrear=new Servicios();
+        RequestContext.getCurrentInstance().execute("PF('widgetNuevoServicio').show()");
+    }
+    
+    public void mensaje()
+    {
+        System.out.println("imprimiendo mensaje...");
+    }
+    
+    public void editarCategoria()
+    {
+        System.out.println("editando la categoria");
+        categoriaServicio.editar(categoriaSeleccionada);
+        categoriaTrabajoList=categoriaServicio.obtenerTodos();
+        RequestContext.getCurrentInstance().execute("PF('widgetEditarServicio').hide()");
         
     }
     
+    public void eliminar(CategoriaTrabajo categoria)
+    {
+        System.out.println("eliminando categoria");
+        categoriaServicio.eliminar(categoria);
+        categoriaTrabajoList=categoriaServicio.obtenerTodos();
+    }
     
-    //////////////////METODOS GET AND SET/////////////////////
+    public void guardarServicio()
+    {
+        System.out.println("grabando nuevo servicio ...");
+        serviciosServicio.grabar(servicioCrear);
+        serviciosList=serviciosServicio.obtenerTodos();
+        RequestContext.getCurrentInstance().execute("PF('widgetNuevoServicio').hide()");
+        
+    }
 
+    //////////////////METODOS GET AND SET/////////////////////
     public List<CategoriaTrabajo> getCategoriaTrabajoList() {
         return categoriaTrabajoList;
     }
@@ -137,8 +177,14 @@ public class CategoriaServiciosMB implements Serializable{
     public void setIdServicioSeleccionado(Integer idServicioSeleccionado) {
         this.idServicioSeleccionado = idServicioSeleccionado;
     }
-    
-    
+    public Servicios getServicioCrear() {
+        return servicioCrear;
+    }
+
+    public void setServicioCrear(Servicios servicioCrear) {
+        this.servicioCrear = servicioCrear;
+    }
+
     
     
 }
